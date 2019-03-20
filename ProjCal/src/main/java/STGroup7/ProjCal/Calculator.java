bbb5ef917f4de9eebd3bbf1bb2ee8602bd28e654
Pranu <Pranu@193.11.186.217>
2019-03-20 17:18:01 +0100

    commit for fail test cases

--- ProjCal/src/main/java/STGroup7/ProjCal/Calculator.java
+++ ProjCal/src/main/java/STGroup7/ProjCal/Calculator.java
@@ -6,16 +6,16 @@ public class Calculator {
 		
 	}
 	public int add(int a, int b){
-		return a+b;
+		return 0;
 	}
 	public int sub(int c,int d) {
-	    return c-d;
+	    return 0;
 	}
 	public int mul(int e,int f) {
-		return e*f;
+		return 0;
 	}
 	public int div(int g,int h) {
-		return g/h;
+		return 0;
 	}
 
 }
