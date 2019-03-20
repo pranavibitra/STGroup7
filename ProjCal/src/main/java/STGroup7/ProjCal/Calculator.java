0d8bce23dcaea89aa2b179ef51f7d0bfa3106e76
Pranu <Pranu@193.11.186.217>
2019-03-20 17:24:50 +0100

    commit modified methods

--- ProjCal/src/main/java/STGroup7/ProjCal/Calculator.java
+++ ProjCal/src/main/java/STGroup7/ProjCal/Calculator.java
@@ -6,16 +6,16 @@ public class Calculator {
 		
 	}
 	public int add(int a, int b){
-		return 0;
+		return a+b;
 	}
 	public int sub(int c,int d) {
-	    return 0;
+	    return c-d;
 	}
 	public int mul(int e,int f) {
-		return 0;
+		return e*f;
 	}
 	public int div(int g,int h) {
-		return 0;
+		return g/h;
 	}
 
 }
