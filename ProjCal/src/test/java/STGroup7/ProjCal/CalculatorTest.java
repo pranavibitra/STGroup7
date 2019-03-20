330239628015609e095cad45e03dbcfa1478a78c
Pranu <Pranu@DESKTOP-7T2RHG8>
2019-03-20 15:18:37 +0100

    commit add

--- ProjCal/src/test/java/STGroup7/ProjCal/CalculatorTest.java
+++ ProjCal/src/test/java/STGroup7/ProjCal/CalculatorTest.java
@@ -13,11 +13,14 @@ public class CalculatorTest {
 	public static void setup() {
 		calc =new Calculator();
 	}
-	
-	
+		
 	@Test
 	public void testadd() {
 		assertEquals(8,calc.add(3, 5));
+	}
+	@Test
+	public void testadd1() {
+		assertEquals(10,calc.add(5, 5));
 	}
 	@Test
 	public void testsub() {
@@ -27,7 +30,6 @@ public class CalculatorTest {
 	public void testmul() {
 		assertEquals(36,calc.mul(4,9));
 	}
-	
 	@Test
 	public void testdiv() {
 		assertEquals(2,calc.div(10,5));
